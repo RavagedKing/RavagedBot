2e4d9782a7a677164d30c457dc385c4df5800732
RavagedKing <darkcloud@live.be>
2024-08-19 07:10:00 +0200

    Update config.js

--- config.js
+++ config.js
@@ -27,9 +27,9 @@ module.exports = {
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
   embedColor: "#1db954",
-  musicardTheme:"themes16", //Goes from themes1 to themes19
-  activityName: "You", // This is bot status Write Anything here 
-  activityType: "WATCHING",  // Available activity types : LISTENING , PLAYING
+  musicardTheme:"themes8", //Goes from themes1 to themes19
+  activityName: "Cooking the end of Times", // This is bot status Write Anything here 
+  activityType: "PLAYING",  // Available activity types : LISTENING , PLAYING
   SupportServer: "https://discord.gg/xQF9f9yUEM",
   CheckmarkIcon: "https://cdn.discordapp.com/attachments/1230824451990622299/1230836684774576168/7762-verified-blue.gif",
   MusicIcon:"https://media.discordapp.net/attachments/1230824451990622299/1236664581364125787/music-play.gif",

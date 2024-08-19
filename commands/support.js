2d48b94af32466d50204467d265741d7b2201db2
RavagedKing <darkcloud@live.be>
2024-08-19 21:11:25 +0200

    Update support.js

--- commands/support.js
+++ commands/support.js
@@ -1,25 +1,4 @@
-/*
-
-  ________.__                        _____.___.___________
- /  _____/|  | _____    ____  ____   \__  |   |\__    ___/
-/   \  ___|  | \__  \ _/ ___\/ __ \   /   |   |  |    |   
-\    \_\  \  |__/ __ \\  \__\  ___/   \____   |  |    |   
- \______  /____(____  /\___  >___  >  / ______|  |____|   
-        \/          \/     \/    \/   \/                  
-
-╔════════════════════════════════════════════════════════════════════════╗
-║                                                                        ║
-║  ## Created by GlaceYT!                                                ║
-║  ## Feel free to utilize any portion of the code                       ║
-║  ## DISCORD :  https://discord.com/invite/xQF9f9yUEM                   ║
-║  ## YouTube : https://www.youtube.com/@GlaceYt                         ║
-║                                                                        ║
-╚════════════════════════════════════════════════════════════════════════╝
-
-
-*/
-
-const { EmbedBuilder } = require('discord.js');
+const { EmbedBuilder, ButtonBuilder, ActionRowBuilder, ButtonStyle } = require('discord.js');
 
 module.exports = {
   name: "support",
@@ -28,43 +7,39 @@ module.exports = {
   options: [],
   run: async (client, interaction) => {
     try {
-
       const XLink = "https://x.com/RavagedWorks";
       const githubLink = "https://github.com/RavagedKing";
-        const embed = new EmbedBuilder()
-            .setColor('#b300ff')
-            .setAuthor({
-              name: 'De rien les gars! ',
-              
-          })
-            .setDescription(`\n🍈 **Follow on:**\n- GitHub - ${githubLink}\n- X - ${XLink}`)
-            .setImage('https://images-ext-1.discordapp.net/external/uS2UFLS7XJsvheFom6cEm3-wUy6L_wLBXIkXwA43n54/https/i.kym-cdn.com/photos/images/newsfeed/002/638/553/929.jpg?format=webp')
-            .setTimestamp();
 
-      return interaction.reply({ embeds: [embed] });
+      const embed = new EmbedBuilder()
+        .setColor('#b300ff')
+        .setAuthor({
+          name: 'De rien les gars!',
+        })
+        .setDescription(`\n🍈 **Follow on:**\n- GitHub - ${githubLink}\n- X - ${XLink}`)
+        .setImage('https://images-ext-1.discordapp.net/external/uS2UFLS7XJsvheFom6cEm3-wUy6L_wLBXIkXwA43n54/https/i.kym-cdn.com/photos/images/newsfeed/002/638/553/929.jpg?format=webp')
+        .setTimestamp();
+
+      const button1 = new ButtonBuilder()
+        .setLabel('Le rêve de Thomas')
+        .setURL('https://i.redd.it/f8maeka25atb1.jpg')
+        .setStyle(ButtonStyle.Link);
+
+      const button2 = new ButtonBuilder()
+        .setLabel('Clique Giannix')
+        .setURL('https://www.youtube.com/watch?v=ZrrQMlUzar8')
+        .setStyle(ButtonStyle.Link);
+
+      const button3 = new ButtonBuilder()
+        .setLabel('Lel')
+        .setURL(XLink)
+        .setStyle(ButtonStyle.Link);
+
+      const row = new ActionRowBuilder().addComponents(button1, button2, button3);
+
+      return interaction.reply({ embeds: [embed], components: [row] });
+      
     } catch (e) {
-    console.error(e); 
-  }
+      console.error(e);
+    }
   },
 };
-
-/*
-
-  ________.__                        _____.___.___________
- /  _____/|  | _____    ____  ____   \__  |   |\__    ___/
-/   \  ___|  | \__  \ _/ ___\/ __ \   /   |   |  |    |   
-\    \_\  \  |__/ __ \\  \__\  ___/   \____   |  |    |   
- \______  /____(____  /\___  >___  >  / ______|  |____|   
-        \/          \/     \/    \/   \/                  
-
-╔════════════════════════════════════════════════════════════════════════╗
-║                                                                        ║
-║  ## Created by GlaceYT!                                                ║
-║  ## Feel free to utilize any portion of the code                       ║
-║  ## DISCORD :  https://discord.com/invite/xQF9f9yUEM                   ║
-║  ## YouTube : https://www.youtube.com/@GlaceYt                         ║
-║                                                                        ║
-╚════════════════════════════════════════════════════════════════════════╝
-
-
-*/
